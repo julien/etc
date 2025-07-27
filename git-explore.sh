bbb7c0feae571f805025cf7c1f4ad141e32d6e1c
julien <julien@users.noreply.github.com>
2025-07-27 04:25:54 +0200

    Add git-explore script
    Explore the git log in vim or nvim.
    
    How:
    Rename git-explore.sh to git-explore, make it executable (chmod +x
    git-explore) and place it in a directory listed in PATH, set EDITOR to nvim
    or vim if not done. Run git explore in any (local) git repository.
    
    Adapted from nrk.

---
+++ git-explore.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
+main() {
+    git rev-parse 2> /dev/null || return 1
+
+    [[ "$EDITOR" =~ n?vim ]] || return 1
+
+    local log="git --no-pager log --oneline --color=always -n 128"
+
+    local opts=( "$@" )
+    if [[ "${#opts[@]}" -eq 0 ]]; then
+        log="$log -n 128"
+    else
+        for opt in "${opts[@]}"; do log="$log $opt"; done;
+    fi
+
+    local cmd="$EDITOR \\
+    '+nnoremap q :bd!<CR>' '+nnoremap Q :qa!<CR>' \\
+    \"+nnoremap <silent> K 0:tabnew \| setfiletype git \| syntax on \| exe 'read! git --no-pager show <C-r><C-w>' \| norm ggdd<CR>\" \\
+    "
+    [[ "$EDITOR" == "nvim" ]] && cmd="$cmd \"+term $log\""
+    [[ "$EDITOR" == "vim"  ]] && \
+        cmd="$cmd \"+call term_start('$log', {'hidden': 1, 'term_finish': 'open', 'term_opencmd': 'buffer %d'})\""
+
+    eval "$cmd"
+}
+
+main "$@"
